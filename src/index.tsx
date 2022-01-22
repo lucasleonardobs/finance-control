3ea70c86f6041831a5b2a05047f3494114d14877
Lucas Leonardo <llbs@cin.ufpe.br>
2022-01-21 22:51:24 -0300

    Setup MirageJS to serve data

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs'; 
 
 import { App } from './App';
 
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get('transactions', () => {
+      return [
+        {
+          id: 1,
+          title: "Transaction 1",
+          type: "Deposit",
+          category: "Food",
+          value: "R$ 25,00"
+        }
+      ]
+    })
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
