9e7a2a946b0ab0affb2e441db43cb51232e2f321
Lucas Leonardo <lucas.leonardo@g.globo>
2022-01-26 15:13:12 -0300

    Change background color

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,7 +3,7 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
     :root {
-        --background: #FEF2F5;
+        --background: #F2F1FF;
         --shape: #FFFFFF;
 
         --red: #E52E40;
