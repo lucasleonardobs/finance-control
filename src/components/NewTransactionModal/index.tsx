f9baf428c2b6b027446e228e3323862510572f0f
Lucas Leonardo <lucas.leonardo@g.globo>
2022-01-26 15:42:53 -0300

    Stylize new transaction modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,10 @@
 import Modal from 'react-modal';
 
-import { Container } from './styles';
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+
+import { Container, TransactionTypeContainer } from './styles';
 
 
 type NewTransactionModalProps = {
@@ -16,9 +20,17 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
             overlayClassName="react-modal-overlay"
             className="react-modal-content"
         >
-            <h2>Cadastrar transação</h2> 
+            <button 
+                type="button" 
+                onClick={onRequestClose} 
+                className="react-modal-close"
+            > 
+                <img src={closeImg} alt="Fechar modal" />
+            </button>
 
             <Container>
+                <h2>Cadastrar transação</h2> 
+
                 <input placeholder='Título' />
                 <input type='number' placeholder='Valor' />
                 <input placeholder='Categoria' />
