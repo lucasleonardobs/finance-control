72c24cc6001ceebb88c1180aa3b60e88db730383
Lucas Leonardo <lucas.leonardo@g.globo>
2022-01-26 15:24:54 -0300

    Create modal form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,17 +1,32 @@
 import Modal from 'react-modal';
 
+import { Container } from './styles';
+
+
 type NewTransactionModalProps = {
     isOpen: boolean;
     onRequestClose: () => void;
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
-    return (      
+    return (
         <Modal 
             isOpen={isOpen}
             onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
         >
             <h2>Cadastrar transação</h2> 
+
+            <Container>
+                <input placeholder='Título' />
+                <input type='number' placeholder='Valor' />
+                <input placeholder='Categoria' />
+
+                <button type="submit">
+                    Cadastrar
+                </button>
+            </Container>
         </Modal>
     );
 };
