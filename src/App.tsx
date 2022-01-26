0266e8a4094c93b1084f939f4b3857e524579faa
Lucas Leonardo <lucas.leonardo@g.globo>
2022-01-26 15:15:01 -0300

    Separe modal in your own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 import Modal from 'react-modal';
 import { useState } from "react";
@@ -23,14 +24,10 @@ export function App() {
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
-
-      <Modal 
-        isOpen={isNewTransactionModalOpen}
-        onRequestClose={handleCloseNewTransactionModal}
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen} 
+          onRequestClose={handleCloseNewTransactionModal}/
       >
-        <h2>Cadastrar transação</h2> 
-      </Modal>
-
       <GlobalStyle />
     </>
   );
