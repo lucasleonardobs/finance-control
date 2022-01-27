a281fd3a1b56d5ec62ba6b3e3536c9781e3b28b9
Lucas Leonardo <lucas.leonardo@g.globo>
2022-01-27 10:44:46 -0300

    Create seed data in API

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer, Model } from 'miragejs'; 
+import { createServer, Model, Server } from 'miragejs'; 
 
 import { App } from './App';
 
@@ -8,6 +8,37 @@ import { App } from './App';
 createServer({
   models: {
     transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Desenvolvimento de Aplicativo',
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          amount: 6000,
+          createdAt: new Date('2022-01-27 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel de apartamento',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1500,
+          createdAt: new Date('2022-01-27 09:00:00')
+        },
+        {
+          id: 3,
+          title: 'Geladeira',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 3500,
+          createdAt: new Date('2022-01-27 09:00:00')
+        },
+      ]
+    })
   },
 
   routes() {
